b301de3c074a2e360ea4033a7b37164a63c0e728
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2022-03-09 17:25:11 +0100

    Ajout de la navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,11 @@
 <template>
+  <NavBar></NavBar>
   <div>
-    <h1>Hello App!</h1>
-    <p>
-      <!-- use the router-link component for navigation. -->
-      <!-- specify the link by passing the `to` prop. -->
-      <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-      <router-link to="/">Accueil</router-link>
-      <br />
-      <router-link to="/api">Api</router-link>
-      <br />
-      <router-link to="/cours">Cours</router-link>
-    </p>
-    <!-- route outlet -->
-    <!-- component matched by the route will render here -->
-    <div class="container">
-      <router-view></router-view>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+</script>
+
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar navbar-light bg-light navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img
+          src="@/assets/logo.png"
+          alt=""
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        Bootstrap
+      </a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Accueil</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/api">Api</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/cours">Cours</router-link>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input
+          class="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.navbar-expand-lg .navbar-nav {
+  flex-direction: row;
+}
+</style>
