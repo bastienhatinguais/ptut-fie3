203eab6c9d260792440f949cc9b4db41e6e2921e
ClotildeSalvador <94382971+ClotildeSalvador@users.noreply.github.com>
2022-03-10 17:23:27 +0100

    modification pour adaptation
    adaptations aux éléments ajoutés

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,25 +4,29 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { createRouter, createWebHistory } from "vue-router";
-import Fragment from "vue-fragment";
+
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import Home from "@/views/Home.vue";
 import Api from "@/views/Api.vue";
 import Cours from "@/views/Cours.vue";
+import Affich from "@/views/Affich.vue";
+import Affichage from "@/views/Affichage.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/api", component: Api },
-  { path: "/cours", component: Cours },
+    { path: "/", component: Home },
+    { path: "/api", component: Api },
+    { path: "/cours/", component: Cours },
+    { path: "/Affich", component: Affich },
+    { path: "/cours/:titre", component: Affichage }
 ];
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = createRouter({
-  history: createWebHistory(),
-  routes: routes,
+    history: createWebHistory(),
+    routes: routes,
 });
 
-createApp(App).use(router).use(Fragment.Plugin).use(Toast).mount("#app");
+createApp(App).use(router).use(Toast).mount("#app");
