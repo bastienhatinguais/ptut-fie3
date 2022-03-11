20bc23f3633c1ced3c8830bbfa4b211a30d5fa32
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2022-03-11 11:46:16 +0100

    Update routes.js
    Ajout de la route de modification

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import Home from "@/views/Home.vue";
 import Api from "@/views/Api.vue";
 import CoursListe from "@/views/cours/Liste.vue";
 import CoursAjouter from "@/views/cours/Ajouter.vue";
+import CoursModifier from "@/views/cours/Modifier.vue";
 
 export default [
   { path: "/", component: Home },
@@ -13,5 +14,10 @@ export default [
   {
     path: "/cours/ajouter",
     component: CoursAjouter,
+  },
+  {
+    path: "/cours/:id/modifier",
+    component: CoursModifier,
+    props: true,
   },
 ];
