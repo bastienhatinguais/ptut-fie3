3dc7d44ab0e983558aeb29d60006216b63276b27
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2022-03-11 11:46:57 +0100

    Ajout de l'edition de cours

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,5 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import router from "./router";
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 
 createApp(App).use(router).use(Toast).mount("#app");
