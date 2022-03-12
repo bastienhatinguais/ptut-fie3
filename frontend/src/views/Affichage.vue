43302e1e6de92a88bd4a625e5afda9b397bf071d
ClotildeSalvador <94382971+ClotildeSalvador@users.noreply.github.com>
2022-03-12 22:38:02 +0100

    rédaction du corps de la vue Affichage

--- frontend/src/views/Affichage.vue
+++ frontend/src/views/Affichage.vue
@@ -1,9 +1,53 @@
 <template>
   <div>
-    <h3>ici on va afficher juste un cours</h3>
+    <h1>ici on va afficher juste un cours</h1>
+    <div class="d-flex align-items-start">
+      <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+        <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Points clefs</button>
+        <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Resume</button>
+        <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Objectifs</button>
+        <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Prerequis</button>
+        <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Competences necessaires</button>
+        <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Contenu pedagogique</button>
+      </div>
+      <div class="tab-content" id="v-pills-tabContent">
+        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+          <h3>{{cours.titre}}</h3>
+        </div>
+        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+          ...
+        </div>
+        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+          ...
+        </div>
+        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+          ...
+        </div>
+        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+          ...
+        </div>
+        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+          ...
+        </div>
+      </div>
+    </div>
   </div>
-
 </template>
 
-<script>
+<script setup>
+
+import { selfLinkToId } from "@/utils";
+import { ref, onMounted } from 'vue'
+import { axiosApi } from "@/api/api";
+
+let cours = ref([]);
+console.log(cours)
+
+onMounted(function () {
+  axiosApi.get("cours").then((response) => {
+    cours.value = response.data._embedded.cours;
+    console.log(response);
+  });
+});
+
 </script>
