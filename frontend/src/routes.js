5bbdcb3300d5032e894fbfdd9cd06b321cff429a
ClotildeSalvador <94382971+ClotildeSalvador@users.noreply.github.com>
2022-04-07 11:49:23 +0200

    ajout de la route vers CoursAffichage
    suite au déplacement des routes vers un dossier dédié par bastien

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,23 +4,25 @@ import CoursListe from "@/views/cours/Liste.vue";
 import CoursAjouter from "@/views/cours/Ajouter.vue";
 import CoursModifier from "@/views/cours/Modifier.vue";
 import Inscription from "./views/Inscription.vue";
+import CoursAffichage from "@/views/cours/Affichage.vue";
 
 export default [
-  { path: "/", component: Home },
-  { path: "/api", component: Api },
-  { path: "/inscription", component: Inscription },
+    { path: "/", component: Home },
+    { path: "/api", component: Api },
+    { path: "/inscription", component: Inscription },
 
-  {
-    path: "/cours",
-    component: CoursListe,
-  },
-  {
-    path: "/cours/ajouter",
-    component: CoursAjouter,
-  },
-  {
-    path: "/cours/:id/modifier",
-    component: CoursModifier,
-    props: true,
-  },
+    {
+        path: "/cours",
+        component: CoursListe,
+    },
+    {
+        path: "/cours/ajouter",
+        component: CoursAjouter,
+    },
+    {
+        path: "/cours/:id/modifier",
+        component: CoursModifier,
+        props: true,
+    },
+    { path: "/cours/:id", component: CoursAffichage, props: true }
 ];
